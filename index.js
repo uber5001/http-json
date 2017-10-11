eb6570bdcb9d346fc5613b1a1ffd3d9e0de5a83e
Scott Little <slittle@nulite-lighting.com>
2017-10-11 10:52:17 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+export default function _h(method, url, payload) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => 
+      xhr.status >= 300
+        ? reject(xhr.statusText)
+        : resolve(xhr.responseText ? JSON.parse(xhr.responseText) : xhr.responseText)
+    
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send(payload ? JSON.stringify(payload) : payload);
+  });
+}
